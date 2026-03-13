28815d511d05816872bb042150f02c6ec85e9cdc
Harshik Dendi <YOUR_GITHUB_EMAIL>
2026-03-13 16:14:09 +0530

    CI/CD pipeline project

---
+++ ci_pipeline.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("CI/CD Pipeline Automation Project")
+
+a = 10
+b = 20
+
+print("Sum =", a + b)
+get_ipython().system('jupyter nbconvert --to script ci_pipeline.ipynb')
+
+
+# In[ ]:
+
+
+
+
